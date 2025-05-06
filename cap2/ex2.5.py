4f02b1f870874d92ebba71bc667b6675ee5af8d6
JoaoCostaTI <joao.costapp@outlook.com>
2025-05-06 15:37:41 -0300

    ex

---
+++ cap2/ex2.5.py
@@ -0,0 +1,7 @@
+valor1 = 3
+valor2 = 4
+valor3 = 5
+
+soma = valor1 + valor2 + valor3
+
+print(soma)
--- cap2/teste.py
+++ cap2/teste.py
@@ -1,2 +1,9 @@
-print('Oi')
-print('Olá Mundo! Me livrei da maldição! ')
+#Programa 2.1 Somando variáveis
+a = 2
+b = 3
+print(a+b)
+
+#Programa 2.2 Calculo de aumento de salário
+salario = 1500
+aumento = 5
+print(salario + (salario*aumento/100))
