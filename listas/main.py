dc608d06ce9a3b03a637d00bb610ecdcef0e7f97
JoaoCostaTI <joao.costapp@outlook.com>
2025-06-22 11:41:27 -0300

    Create main.py

---
+++ listas/main.py
@@ -0,0 +1,14 @@
+def listaDeAlgo(lista):
+    for i in lista:
+        print(i)
+    return lista
+
+
+
+algo = ['Algo', 'Aqui', 'Vai', 'Acontecer']
+
+listaDeAlgo(algo)
+
+lista2 = listaDeAlgo(algo)
+
+print(lista2)
